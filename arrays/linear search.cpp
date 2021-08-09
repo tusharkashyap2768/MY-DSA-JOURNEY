e474265efc19366282fcfffafd4a3787aa4f5dda
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-08-09 12:15:16 +0530

    Update linear search.cpp

--- arrays/linear search.cpp
+++ arrays/linear search.cpp
@@ -1,1 +1,30 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int lsearch(int arr[],int n, int key)
+{
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] == key)
+        {
+            return i;
+        }
+    } 
+    return -1;
+}
+main()
+{
+    int a[]={1,2,3,4,5,6,7};
+    int n =sizeof(a)/sizeof(int);
+    int key=0;
+    cin>>key;
+    int index = lsearch(a,n,key);
+    if(index !=0)
+    {
+        cout<<"elemenet is present at" <<index<<endl;
+    }
+    else
+    {
+        cout<<"elemenet is not present";
+    }
+}
