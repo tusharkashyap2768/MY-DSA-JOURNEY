4999a314f4df98623e77eef87adeedd6701c0ba9
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-08-10 13:05:34 +0530

    Update print pairs.cpp

--- arrays/print pairs.cpp
+++ arrays/print pairs.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int pairs (int a[],int n)
+{
+   for(int i=0; i<n; i++)
+   {
+       int x=a[i];
+       for(int j=i+1; j<n; j++)
+       {
+           int y=a[j];
+           cout<<x<<","<<y<<endl;
+       }
+       cout<<endl;
+   }
 
+}
+main()
+{
+    int a[]={1,2,3,4,5,6,7}; 
+    int n =sizeof(a)/sizeof(int);
+     pairs(a,n);
+
+}
