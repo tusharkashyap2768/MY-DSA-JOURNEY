f78a48efc02ba6437b905d2a1863bbf4ca314662
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-08-10 14:16:25 +0530

    Update print all subarrays.cpp

--- arrays/print all subarrays.cpp
+++ arrays/print all subarrays.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int subarrays (int a[],int n)
+{
+   for(int i=0; i<n; i++)
+   {
+       for(int j=i; j<n; j++)
+       {
+           for(int k=i; k<=j; k++)
+           {
+               cout<<a[k]<<",";
+           }
+            cout<<endl;
+       }
+      
+   }
 
+}
+main()
+{
+    int a[]={1,2,3,4,5,6,7}; 
+    int n =sizeof(a)/sizeof(int);
+     subarrays(a,n);
+
+}
