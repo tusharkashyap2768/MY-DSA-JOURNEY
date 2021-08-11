e98dbb575c5d6bb6cc85674c768e4a15c7ee6d03
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-08-11 13:54:39 +0530

    Update insertion sort.cpp

--- searching and sorting/insertion sort.cpp
+++ searching and sorting/insertion sort.cpp
@@ -1,1 +1,29 @@
+// insertion sort is o(n^2) //
+#include<bits/stdc++.h>
+using namespace std;
+void insertionsort (int a[],int n)
+{
 
+    for(int i = 1; i < n-1; i++)
+    {
+         int current =a[i];
+         int previous =i-1;
+         while(previous >=0 and a[previous] > current) 
+         {
+           a[previous+1]=a[previous];
+           previous=previous-1;
+         }
+
+          a[previous+1]=current;
+    }
+
+}
+main()
+{
+    int a[]={-2,3,4,-1,5,-12,6,1,3}; 
+    int n =sizeof(a)/sizeof(int);
+    insertionsort(a,n);
+    for(auto x: a)
+    cout<<x<<" ";
+
+}
