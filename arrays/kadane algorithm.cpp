0e9dd70ab83f09e293a4ce5cf9645772193dc146
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-08-11 12:23:25 +0530

    Update kadane algorithm.cpp

--- arrays/kadane algorithm.cpp
+++ arrays/kadane algorithm.cpp
@@ -1,1 +1,25 @@
+// kadane's algorithm is o(n) //
+#include<bits/stdc++.h>
+using namespace std;
+int largestsubarraysum (int a[],int n)
+{int largestsum=0;
+int cs=0;
+   for(int i=0; i<n; i++)
+   {
+      cs=cs+a[i];
+      if(cs<0)
+      {
+          cs=0;
+      }
+       largestsum=max(largestsum,cs);
+   }
+   return largestsum;
 
+}
+main()
+{
+    int a[]={-2,3,4,-1,5,-12,6,1,3}; 
+    int n =sizeof(a)/sizeof(int);
+    cout<<largestsubarraysum(a,n)<<endl;
+
+}
