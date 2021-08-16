b4c13ad7c67789018148d7a9b7805a9b5ec9a447
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-08-16 15:06:46 +0530

    Update selection sort.cpp

--- searching and sorting/selection sort.cpp
+++ searching and sorting/selection sort.cpp
@@ -1,1 +1,31 @@
+// selection sort is o(n^2) //
+#include<bits/stdc++.h>
+using namespace std;
+void selectionsort (int a[],int n)
+{
 
+    for(int position = 0; position <= n-2; position++)
+    {
+         int current =a[position];
+         int min_position =position;
+         for(int j=position;j<n;j++)
+         {
+             if(a[j] < a[min_position])
+             {
+                 min_position = j;
+             }
+         }
+         swap(a[min_position],a[position]);;
+
+    }
+
+}
+main()
+{
+    int a[]={-2,3,4,-1,5,-12,6,1,3}; 
+    int n =sizeof(a)/sizeof(int);
+    selectionsort(a,n);
+    for(auto x: a)
+    cout<<x<<" ";
+
+}
