8f649faf6f01737502fdc2453f784fd97a5dcf9b
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-08-16 19:25:08 +0530

    Update count sort.cpp

--- searching and sorting/count sort.cpp
+++ searching and sorting/count sort.cpp
@@ -1,1 +1,39 @@
+// o(n)+R
 
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+void counting_sort(int a[],int n){
+	//Largest Element
+	int largest = -1;
+
+	for(int i=0;i<n;i++){
+		largest = max(largest,a[i]);
+	}
+
+	//create a count array/vector
+	vector<int> freq(largest+1,0);
+
+	//Update the freq array 
+	for(int i=0;i<n;i++){
+		freq[a[i]]++;
+	}
+
+	//Put back the elements from freq into original array
+
+	int j = 0;
+	for(int i=0; i<=largest; i++){
+
+		while(freq[i]>0){
+			a[j] = i;
+			freq[i]--;
+			j++;
+		}
+	}
+	return;
+
+}
