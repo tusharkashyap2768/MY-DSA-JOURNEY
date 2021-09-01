a0512c66f4099b599ac25126f21f4463915aa85b
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-09-01 16:58:21 +0530

    Update insert at bottom of stack.cpp

--- stacks/insert at bottom of stack.cpp
+++ stacks/insert at bottom of stack.cpp
@@ -1,1 +1,32 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+void insertatbottom(stack<int>&s,int data)
+{
+if(s.empty()) 
+{
+	s.push(data);
+	return;
+}
+
+int temp =s.top();
+s.pop();
+insertatbottom(s,data);
+s.push(temp);
+}
+
+int main()
+{
+	stack<int>s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.push(4);
+	insertatbottom(s,5);
+	while(!s.empty())
+	{
+		cout<<s.top()<<endl;
+        s.pop();
+	}
+	return 0;
+}
