b4b3ae86a4f1cb94a443553574931e5e30b288b4
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-09-07 11:18:40 +0530

    Update levelorder.cpp

--- trees/levelorder.cpp
+++ trees/levelorder.cpp
@@ -1,1 +1,72 @@
 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+  public : int data;
+  Node*left;
+  Node*right;
+  Node(int d){
+  data=d;
+  left=right=NULL;
+}};
+
+Node*buildtree()
+{
+  int d;
+  cin>>d;
+  if(d==-1)
+  {
+    return NULL;
+  }
+  Node * n = new Node(d);
+  n->left=buildtree();
+  n->right=buildtree();
+  return n;
+}
+
+void levelorder(Node *root)
+{
+queue<Node*> q;
+q.push(root);
+q.push(NULL);
+
+while(!q.empty()){
+   Node * temp=q.front();
+   if(temp==NULL)
+   {
+    cout<<endl;
+    q.pop();
+
+    //insert new null for next level
+    if(!q.empty())
+    {
+      q.push(NULL);
+    }
+   }
+   else
+   {
+     q.pop();
+     cout<<temp->data<<" ";
+     if(temp->left)
+     {
+       q.push(temp->left);
+     }
+
+     if(temp->right)
+     {
+       q.push(temp->right);
+     }
+   }
+}
+return;
+}
+
+
+int main()
+{
+ Node *root=buildtree();
+ levelorder(root);
+}
+
