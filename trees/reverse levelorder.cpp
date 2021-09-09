b89e0607a1afe9a29a373d91a8dc0d3ffbcc82c6
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-09-09 11:54:05 +0530

    Update reverse levelorder.cpp

--- trees/reverse levelorder.cpp
+++ trees/reverse levelorder.cpp
@@ -1,1 +1,27 @@
 
+vector<int> reverseLevelOrder(Node *root)
+{
+    vector<int>v;
+    if(!root)
+    return ;
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        Node*t=q.front();
+        v.push_back(t->data);
+        if(root->right)
+        {
+            q.push(root->right);
+        }
+        if(root->left)
+        {
+            q.push(root->left);
+        }
+        q.pop();
+    }
+    reverse(v.begin(),v.end());
+    return v;
+    for(int i=0;i<v.size();i++)
+    cout<<v[i]<<" ";
+}
