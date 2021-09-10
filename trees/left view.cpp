fdfddcae42da286a26ad158677cf7373f3708ec6
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-09-10 12:14:20 +0530

    Update left view.cpp

--- trees/left view.cpp
+++ trees/left view.cpp
@@ -1,1 +1,28 @@
 
+vector<int> leftView(Node *root)
+{
+   vector<int> v;
+   if(!root)
+   return v;
+   queue<Node*>q;
+   q.push(root);
+   while(!q.empty())
+   {
+       v.push_back(q.front()->data);
+       int size=q.size();
+       while(size--)
+       {
+           Node *t=q.front();
+           q.pop();
+           if(t->left)
+           {
+            q.push(t->left);
+           }
+           if(t->right)
+           {
+            q.push(t->right);
+           }
+       }
+       return v;
+   }
+}
