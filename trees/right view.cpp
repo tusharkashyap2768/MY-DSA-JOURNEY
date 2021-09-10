42d2a711bd3686c98c9d1c4041df0ba467308686
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-09-10 12:54:14 +0530

    Update right view.cpp

--- trees/right view.cpp
+++ trees/right view.cpp
@@ -1,1 +1,62 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+  public : int data;
+  Node*left;
+  Node*right;
+  Node(int d){
+  data=d;
+  left=right=NULL;
+}};
+
+Node*buildtree()
+{
+  int d;
+  cin>>d;
+  if(d==-1)
+  {
+    return NULL;
+  }
+  Node * n = new Node(d);
+  n->left=buildtree();
+  n->right=buildtree();
+  return n;
+}
+void rightView(Node *root)
+{
+   if(!root)
+   return ;
+   queue<Node*>q;
+   q.push(root);
+   while(!q.empty())
+   {
+       int size=q.size();
+        Node *t;
+       while(size--)
+       {
+           t=q.front();
+           q.pop();
+           if(t->left)
+           {
+            q.push(t->left);
+           }
+           if(t->right)
+           {
+            q.push(t->right);
+           }
+       }
+        cout<<t->data;
+   }
+}
+
+
+int main()
+{
+ Node *root=buildtree();
+//  levelorder(root);
+//  cout <<height(root);
+ rightView(root);
+}
+
