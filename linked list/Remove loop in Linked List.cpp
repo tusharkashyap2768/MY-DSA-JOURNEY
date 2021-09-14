400f718db05f767b61d79167816643ec0178096b
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-09-14 13:27:31 +0530

    Create Remove loop in Linked List.cpp

---
+++ linked list/Remove loop in Linked List.cpp
@@ -0,0 +1,39 @@
+
+class Solution
+{
+    public:
+    //Function to remove a loop in the linked list.
+    void removeLoop(Node* head)
+    {
+       Node * low=head;
+       Node* high=head;
+       while(low!=NULL and high != NULL and high->next !=NULL)
+       {
+           low=low->next;
+           high=high->next->next;
+           if(low==high)
+           break;
+       }
+       //corner case
+       if(low==head)
+       {
+           while(high->next !=low)
+           {
+               high=high->next;
+           }
+           high->next=NULL;
+       }
+       
+       else if(low==high)
+       {
+           low=head;
+           while(low->next!=high->next)
+           {
+               low=low->next;
+               high=high->next;
+           }
+           high->next=NULL;
+       }
+       
+    }
+};
