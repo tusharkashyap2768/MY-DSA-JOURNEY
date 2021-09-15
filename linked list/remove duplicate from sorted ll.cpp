c00cdae73b4f0a12909cdc30e9705ac827bd5c9d
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-09-15 14:16:50 +0530

    Update remove duplicate from sorted ll.cpp

--- linked list/remove duplicate from sorted ll.cpp
+++ linked list/remove duplicate from sorted ll.cpp
@@ -1,1 +1,18 @@
 
+Node *removeDuplicates(Node *root)
+{
+    Node * head= root;
+ if(!head)
+ return NULL;
+ 
+ while(head-> next!=NULL)
+ {
+     if(head->data == head -> next -> data)
+     {
+         head->next =head->next->next;
+     }
+     else
+     head=head->next;
+ }
+ return root;
+}
