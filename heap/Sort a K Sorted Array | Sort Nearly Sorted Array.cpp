f97e3f72f1a44fed66725daf0524414a10d0dfdc
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-09-20 11:55:58 +0530

    Update Sort a K Sorted Array | Sort Nearly Sorted Array.cpp

--- heap/Sort a K Sorted Array | Sort Nearly Sorted Array.cpp
+++ heap/Sort a K Sorted Array | Sort Nearly Sorted Array.cpp
@@ -1,1 +1,21 @@
 
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a[7] = {6, 5, 3, 2, 8, 10, 9};
+    int k = 3;
+    priority_queue<int, vector<int>, greater<int>>min_h;
+    for(int i=0;i<7;i++) {
+        min_h.push(a[i]);
+        if(min_h.size()>k) {
+            cout<<min_h.top()<<endl;
+            min_h.pop();
+        }
+    }
+    while(min_h.size()!=0) {
+        cout<<min_h.top()<<endl;
+        min_h.pop();
+    }
+}
