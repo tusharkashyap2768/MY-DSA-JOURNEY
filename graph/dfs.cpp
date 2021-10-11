41b3e3a77dd1d3f0210cc598f033344af46ea3cb
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-10-11 12:53:52 +0530

    Update dfs.cpp

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -1,1 +1,44 @@
 
+DFS
+#include <bits/stdc++.h>
+using namespace std;
+vector <int> v[5];
+int visited[5];
+void dfs(int source);
+int main() {
+	int n,m,x,y;
+    cin>>n>>m;
+    for(int i=1;i<m;i++)
+    {
+        cin>>x>>y;
+        v[x].push_back(y);
+         //v[y].push_back(x);//undirected
+    }
+    dfs(0);
+    cout<<endl;
+}
+void dfs(int current )
+{
+    cout<<current <<" ";
+    visited[current]=1;
+    for(int i=0;i<v[current].size();i++)
+    {
+        int child =v [current][i];
+        if(visited[child]==0)
+        {
+            dfs(child);
+        }
+    }
+
+}
+/*input
+5 5
+
+0 2
+2 1
+2 4
+4 1
+1 3
+output
+0 2 1 4 
+*/
