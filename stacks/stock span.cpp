dd30f6d1e65c085a4f89281520b6d17593a6c3f9
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-10-28 11:15:09 +0530

    Update stock span.cpp

--- stacks/stock span.cpp
+++ stacks/stock span.cpp
@@ -1,43 +1,32 @@
 o(N)
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-void stockspan(int price[],int n,int span[])
-{
-  stack<int> s;
-  s.push(0);
-  span[0]=1;
-  for(int i=0;i<n;i++)
-  {
-    int currentprice = price[i];
-    while(!s.empty() and price[s.top()]<=currentprice)
-    {
-      s.pop();
-    }  
 
-    if(!s.empty())
-    {
-      int previous=s.top();
-      span[i]=i-previous;
+int main() {
+    int n = 7;
+    int a[n] = {100,80,60,70,60,75,85};
+    vector<int>v;
+    stack<pair<int,int>>s;
+    for(int i=0;i<n;i++) {
+        if(s.size()==0) {
+            v.push_back(-1);
+        } else if(s.size()>0 && s.top().first>a[i]) {
+            v.push_back(s.top().second);
+        } else if(s.size()>0 && s.top().first<=a[i]) {
+            while(s.size()>0 && s.top().first<=a[i]) {
+                s.pop();
+            }
+            if(s.size()==0) {
+                v.push_back(-1);
+            } else {
+                v.push_back(s.top().second);
+            }
+        }
+        s.push({a[i],i});
     }
-    else{
-      span[i]=i+1;
+    for(int i=0;i<n;i++) {
+        cout<<i-v[i]<<endl;
     }
-    s.push(i);
-  }
-}
-
-
-
-int main()
-{
-int price[]={100,80,60,70,60,75,85};
-int n=sizeof(price)/sizeof(int);
-int span[100000]={0};
-stockspan(price,n,span);
-for(int i=0;i<n;i++)
-{
-  cout<<span[i]<<" ";
-}
-return 0;
 }
