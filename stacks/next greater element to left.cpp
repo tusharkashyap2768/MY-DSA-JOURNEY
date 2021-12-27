8445f0a39340c593a95de6247ec08a3f0c6675d6
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-12-27 22:16:59 +0530

    Update next greater element to left.cpp

--- stacks/next greater element to left.cpp
+++ stacks/next greater element to left.cpp
@@ -7,6 +7,7 @@ int main() {
     int a[n] = {1,3,0,0,1,2,4};
     vector<int>v;
     stack<int>s;
+    //yha loop change hui hai
     for(int i=0;i<=n;i++) {
         if(s.size()==0) {
             v.push_back(-1);
@@ -25,6 +26,7 @@ int main() {
         }
         s.push(a[i]);
     }
+    //yha reverse nhi karna hai
     for(int i=0;i<n;i++) {
         cout<<v[i]<<" ";
     }
