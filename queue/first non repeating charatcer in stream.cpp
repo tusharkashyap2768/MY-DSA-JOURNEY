f9977c3c53930d956c28f2958a2ddbe155a34ad7
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2022-02-14 22:03:49 +0530

    Create first non repeating charatcer in stream.cpp

---
+++ queue/first non repeating charatcer in stream.cpp
@@ -0,0 +1,31 @@
+class solution {
+  public:
+		string FirstNonRepeating(string A){
+		    unordered_map<char,int> count;
+		    queue<int>q;
+		    string ans ="";
+		     
+		     for(int i= 0;i<A.length();i++)
+		     {
+		         char ch =A[i];
+		         
+		         count[ch]++;
+		         q.push(ch);
+		         
+		         while(!q.empty())
+		         {
+		             if(count[q.front()] > 1 )
+		             {
+		                 q.pop();
+		             }
+		             else
+		             {
+		                 ans.push_back(q.front());
+		                 break;
+		             }
+		         }
+		         if(q.empty())
+		         ans.push_back('#');
+		     }
+		     return ans;
+		}
