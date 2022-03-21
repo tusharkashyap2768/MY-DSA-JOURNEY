4d09c83ae2930242c178696bd0bf251d182c3346
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2022-03-21 10:17:06 +0530

    Create 3. knapsack topdown.cpp

---
+++ Dynamic Programming/3. knapsack topdown.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 3;
+    int wt[] = { 10, 20, 30 };
+    int val[] = { 60, 100, 120 };
+    int W = 50;
+    int dp[n+1][W+1];
+    for(int i=0;i<n+1;i++)
+        for(int j=0;j<W+1;j++)
+            if(i==0 || j==0)
+                dp[i][j] = 0;
+    for(int i=1;i<n+1;i++)
+        for(int j=1;j<W+1;j++)
+            if(wt[i-1]<=j)
+                dp[i][j] = max(val[i-1]+ dp[i-1][j-wt[i-1]], dp[i-1][j]);
+              else 
+                dp[i][j] = dp[i-1][j];
+    cout<<dp[n][W]<<endl;
+}
