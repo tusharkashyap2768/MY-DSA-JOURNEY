b90cb5009e518d67cd5f33b94a21971aba1b0161
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2022-03-27 00:13:59 +0530

    Create 21.  Minimum number of deletion in a string to make it a palindrome.cpp

---
+++ Dynamic Programming/21.  Minimum number of deletion in a string to make it a palindrome.cpp
@@ -0,0 +1,31 @@
+
+int lcs( string s1, string s2,int x, int y)
+   {
+       // your code here
+       int t[x+1][y+1];
+       int i,j;
+       
+       for(i=0; i<x+1; i++)
+       for(j=0; j<y+1; j++)
+           if( i==0 || j==0)
+               t[i][j]=0;
+       
+       for(i=1; i<x+1; i++)
+       for(j=1; j<y+1; j++)
+       {
+           if(s1[i-1] == s2[j-1])
+               t[i][j] = 1+t[i-1][j-1];
+           else
+               t[i][j] = max(t[i-1][j], t[i][j-1]);
+       }
+       
+       return t[x][y];
+} 
+
+int minDeletions(string str, int n) { 
+   //complete the function here 
+   string a =str;
+   reverse(str.begin(), str.end());
+       
+       return n-lcs(a, str, n, n);
+} 
