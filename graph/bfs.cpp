205b67e523b4f567e33493fdefc6103c60bb1f70
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2022-06-01 18:22:06 +0530

    Update bfs.cpp

--- graph/bfs.cpp
+++ graph/bfs.cpp
@@ -1,41 +1,28 @@
 
 
 BFS
-  #include <bits/stdc++.h>
-using namespace std;
-vector <int> v[5];
-int visited[5];
-void bfs(int source);
-int main() {
-	int n,m,x,y;
-    cin>>n>>m;
-    for(int i=1;i<=m;i++)
-    {
-        cin>>x>>y;
-        v[x].push_back(y);
-         //v[y].push_back(x);//undirected
-    }
-    bfs(0);
-}
-void bfs(int source )
-{
-    queue<int> q;
-    q.push(source);
-    visited[source]=1;
-    while(!q.empty())
-    {
-        int current=q.front();
-        q.pop();
-        cout<<current<<" ";
-    for(int i=0;i<v[current].size();i++)
-    {
-        int child =v [current][i];
-        if(visited[child]==0)
-        {
-            q.push(child);
-            visited[child]=1;
-        }
-    }}
-    cout<<endl;
-
-}
+ 
+ class Solution {
+public:
+	vector<int>bfsOfGraph(int V, vector<int> adj[]){
+	    vector<int> bfs; 
+	    vector<int> vis(V, 0); 
+	    queue<int> q; 
+	    q.push(0); 
+	    vis[0] = 1; 
+	    while(!q.empty()) {
+	        int node = q.front();
+	        q.pop(); 
+	        bfs.push_back(node); 
+	        
+	        for(auto it : adj[node]) {
+	            if(!vis[it]) {
+	                q.push(it); 
+	                vis[it] = 1; 
+	            }
+	        }
+	    }
+	    
+	    return bfs; 
+	}
+};
